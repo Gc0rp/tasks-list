8e68a3c923c433d536da50b2ed3f1972a27fd265
Gc0rp <ojasgupta4321@gmail.com>
2020-07-23 09:10:57 +1000

    Added functionality to add tasks and started programming to allow the user to remove tasks. Cleaned up task-list CSS..

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 const BTN_ADD_TASK = document.querySelector('.js-btn-add-task');
+const TASK_LIST = document.querySelector('.tasks-list');
 
+var todoTasks = [];
 
 function addTask(enteredTask){
 
     let groupTask = document.createElement('div');
-    let form = document.querySelector('.tasks-list');
+    groupTask.setAttribute('class', 'task');
     
+    let form = document.querySelector('.tasks-list');
+
     let checkBox = document.createElement('input');
     checkBox.setAttribute('type', 'checkbox');
-    checkBox.setAttribute('id', enteredTask);
-
+    checkBox.setAttribute('class', enteredTask);
 
     let label = document.createElement('label');
-
     label.setAttribute('for', enteredTask);
     label.innerText = enteredTask;
     
     groupTask.appendChild(checkBox);
     groupTask.appendChild(label);
 
-    console.log(groupTask);
     form.appendChild(groupTask);
-
 }
 
 
 
-BTN_ADD_TASK.addEventListener('click', function buttonClicked(){
+BTN_ADD_TASK.addEventListener('click', function buttonClicked () {
     let enteredTask = document.querySelector('.task');
     addTask(enteredTask.value);
 });
+
+TASK_LIST.addEventListener('click', function taskCompleted(event) {
+    if(event.target.tagName === 'INPUT'){
+    } else if (event.target.tagName === 'LABEL') {
+    }
+});
+
+
+
